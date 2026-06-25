89c61ef1024977de73d4d321da464520b7b87d4f
Rafael Neves <rafael.neves.souzaa@gmail.com>
2026-06-25 09:06:28 -0300

    fix: set base to /app-dolce-neves/

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/app-dolce-neves/',
   plugins: [react()],
   resolve: {
     alias: {
